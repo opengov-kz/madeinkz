4b0b8d5325222493bfb09e6694dd1b1e796d3db4
Morozqq <Aslan.dzheleubaj.04@gmail.com>
2025-03-16 21:06:45 +0500

    feat: script for parsing data
    feat: script for parsing data from atameken and saving it in csv

---
+++ project/scripts/parse_atameken.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import requests
+import io
+import re
+import chardet
+import os
+
+def parse_public_google_sheet(spreadsheet_id, sheet_id=0):
+    url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format=csv&gid={sheet_id}"
+    
+    try:
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            encoding_result = chardet.detect(response.content)
+            detected_encoding = encoding_result['encoding']
+            
+            try:
+                df = pd.read_csv(io.StringIO(response.content.decode(detected_encoding)), low_memory=False)
+            except UnicodeDecodeError:
+                df = pd.read_csv(io.StringIO(response.content.decode('utf-8', errors='replace')), low_memory=False)
+            
+            print(f"Успешно получены данные из таблицы. Всего строк: {len(df)}")
+            return df
+        else:
+            print(f"Ошибка при получении данных. Код статуса: {response.status_code}")
+            return None
+    
+    except Exception as e:
+        print(f"Произошла ошибка при парсинге таблицы: {str(e)}")
+        return None
+
+def clean_data(df):
+    df = df.dropna(how='all')
+    df = df.drop_duplicates()
+    df.columns = [col.strip() for col in df.columns]
+    df = df.dropna(axis=1, how='all')
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    
+    def clean_text(x):
+        if isinstance(x, str):
+            x = re.sub(r'[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]', '', x)
+            return x
+        return x
+    
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].apply(clean_text)
+    
+    return df
+
+def save_data(df, output_file):
+    if df is not None:
+        try:
+            output_dir = "results"
+            os.makedirs(output_dir, exist_ok=True)
+            output_path = os.path.join(output_dir, output_file)
+            df.to_csv(output_path, index=False, encoding='utf-8')
+            print(f"Данные сохранены в файл: {output_path}")
+        except Exception as e:
+            print(f"Ошибка при сохранении файла: {str(e)}")
+
+def main():
+    spreadsheets = [
+        {"id": "1zmFxboRQEAhtE3_ZLJZo6zfrhhKU2z7uw2cTpeiBpyg", "sheets": [1020960867]},
+        {"id": "1sU3hRnkRwd4vEX1GHEGNHzhl6LAeCEvlW88cVweRKZI", "sheets": [1555544843]},
+        {"id": "15RLiTy-Q1NIzR6LE5rS53NYH9oCU5hnkFMIg4S4GNuU", "sheets": [103339423]},
+        {"id": "1fF4Hikljar-u1Vcd9oE_YCEg6wxH3RrdRNITzPA0tdY", "sheets": [485143404]},
+        {"id": "1SQzdOYiACYRSLJszpG_bbk9p3qmq5K-ihjlCbjeYuCI", "sheets": [491961302]},
+        {"id": "1M1IjQZ-_yWRxUf8r0CWqy6eR0f03s5VeCzdpA0R6Gyg", "sheets": [1833732016]},
+        {"id": "1v_qgEvSJFWl0h7_dLEgVtduHZ70uJ81EGczFm1Znzak", "sheets": [1230136339]}
+
+    ]
+    
+    print("Парсинг нескольких таблиц Google Sheets")
+    
+    for i, spreadsheet in enumerate(spreadsheets, start=1):
+        spreadsheet_id = spreadsheet["id"]
+        
+        for j, sheet_id in enumerate(spreadsheet["sheets"], start=1):
+            print(f"\nОбрабатываем таблицу {i}, лист {j} (ID: {spreadsheet_id}, Sheet ID: {sheet_id})")
+            
+            df = parse_public_google_sheet(spreadsheet_id, sheet_id)
+            
+            if df is not None:
+                df = clean_data(df)
+                save_data(df, f'stkz_certificates_{i}_{j}.csv')
+    
+    print("\nВсе данные успешно обработаны и сохранены.")
+
+if __name__ == "__main__":
+    main()
